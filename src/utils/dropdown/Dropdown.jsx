6889c67776f286c615b05aa10be562220fd4f8a2
Ali nadhum <alinadhum1998@gmail.com>
2025-04-22 18:32:02 +0300

    update DropdownMenu

--- src/utils/dropdown/Dropdown.jsx
+++ src/utils/dropdown/Dropdown.jsx
@@ -6,16 +6,16 @@ import { MdOutlinePassword } from "react-icons/md";
 
 const Dropdown = () => {
   return (
-    <div className="absolute right-0 mt-2 w-44 bg-white shadow-lg border border-gray-200 rounded-md z-50 p-3 flex items-center justify-center flex-col">
-      <Link to={""} className="flex w-40 items-center justify-between  mt-1.5">
+    <div className="absolute right-0 mt-4 w-44 bg-white shadow-lg border border-gray-200 rounded-md z-50 p-3 flex items-center justify-center flex-col">
+      <Link to={""} className="flex w-40 items-center justify-between  mt-1.5 hover:bg-gray-100 py-1 rounded p-1.5" dir="rtl">
         <p>الاعدادات</p>
         <CiSettings />
       </Link>
-      <Link to={""} className="flex w-40 items-center justify-between  mt-1.5">
+      <Link to={""} className="flex w-40 items-center justify-between  mt-1.5 hover:bg-gray-100 py-1 rounded p-1.5" dir="rtl">
         <p>تغير كلمه المرور</p>
         <MdOutlinePassword />
       </Link>
-      <Link to={""} className="flex w-40 items-center justify-between mt-1.5">
+      <Link to={""} className="flex w-40 items-center justify-between mt-1.5 hover:bg-gray-100 py-1 rounded p-1.5" dir="rtl">
         <p>حذف الحساب</p>
         <MdDelete />
       </Link>
