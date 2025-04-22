c78924061c34c5b0f99a8da9d8debb6f29f6039d
Ali nadhum <alinadhum1998@gmail.com>
2025-04-22 16:30:47 +0300

    end header v1

--- src/utils/Navbar/Navbar.jsx
+++ src/utils/Navbar/Navbar.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React from "react";
 import logo from "../../assets/logo.png";
+import avatar from "../../assets/avatar.svg";
+import Container from "../../components/Container/Container";
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
+
 
 const Navbar = () => {
-    return (
-        <nav>
+  const cartItemCount = 6;
+  return (
+    <nav className="navbar p-2.5 h-20 w-full flex items-center shadow-md shadow-gray-400/30">
+      <Container>
+        <div className="flex items-center justify-between">
+          <div className="left">
+            <Link> <img src={logo} alt="logo" className="w-16 h-16" /> </Link>
+          </div>
+          <div className="center">
+            <ul className="flex items-center justify-between gap-4">
+              <li> <Link to={"/"} className="font-medium">المفضله</Link> </li>
+              <li> <Link to={"/"} className="font-medium">طلباتي</Link> </li>
+              <li> <Link to={"/"} className="font-medium">الرئيسيه</Link> </li>
+            </ul>
+          </div>
+          <div className="right relative flex items-center justify-between gap-8">
+            <Link to={"/cart"} className="relative">
+              <FiShoppingCart className="text-2xl" />
+              {cartItemCount > 0 && (
+                <span className="absolute -top-0.5 -right-4 bg-red-600 text-white text-xs font-bold rounded-full px-1.5">
+                  {cartItemCount}
+                </span>
+              )}
+            </Link>
+            <img src={avatar} alt="" className="w-11 h-11 bg-amber-400" />
+          </div>
+        </div>
+      </Container>
+    </nav>
+  );
+};
 
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
