d40c50c397deef7c3843513275688a5b9c626c8c
Ali nadhum <alinadhum1998@gmail.com>
2025-04-23 16:39:08 +0300

    add Carousel code

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import Container from "../../components/Container/Container";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const images = [
+  "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/gallery/slide1.png",
+  "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/gallery/slide2.png",
+  "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/gallery/slide3.png",
+  "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/gallery/slide4.png"
+];
 
 
 const HomePage = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <div className='home'>
       <Container>
@@ -12,6 +32,17 @@ const HomePage = () => {
         <div className='flex items-center justify-center'>
        <SearchBar/>
         </div>
+        {/* ===== Carousel ===== */}
+        <div className="w-full max-w-sm sm:max-w-md md:max-w-xl lg:max-w-4xl xl:max-w-5xl mx-auto mt-10">
+      <Slider {...settings}>
+        {images.map((src, idx) => (
+          <div key={idx}>
+            <img src={src} alt={`Slide ${idx}`} className="w-full h-[300px] object-cover rounded-lg" />
+          </div>
+        ))}
+      </Slider>
+    </div>
+        
       </Container>
     </div>
   )
