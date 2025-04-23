0698407659a98193bcb99f45f77af333903a6116
Ali nadhum <alinadhum1998@gmail.com>
2025-04-23 14:57:01 +0300

    update Dropdown comp

--- src/utils/dropdown/Dropdown.jsx
+++ src/utils/dropdown/Dropdown.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { CiSettings } from "react-icons/ci";
-import { MdDelete } from "react-icons/md";
-import { MdOutlinePassword } from "react-icons/md";
+import { MdOutlinePassword , MdDelete , MdOutlineLogout } from "react-icons/md";
 
 const Dropdown = () => {
+
+  const handleLogout = () => {
+   console.log("logout");
+   
+  };
   return (
     <div className="absolute right-0 mt-4 w-44 bg-white shadow-lg border border-gray-200 rounded-md z-50 p-3 flex items-center justify-center flex-col">
       <Link to={""} className="flex w-40 items-center justify-between  mt-1.5 hover:bg-gray-100 py-1 rounded p-1.5" dir="rtl">
@@ -19,6 +23,14 @@ const Dropdown = () => {
         <p>حذف الحساب</p>
         <MdDelete className="text-red-600" />
       </Link>
+      <button
+        onClick={handleLogout}
+        className="flex w-40 items-center justify-between mt-1.5 hover:bg-gray-100 py-1 rounded p-1.5 text-red-500 cursor-pointer"
+        dir="rtl"
+      >
+        <p>تسجيل الخروج</p>
+        <MdOutlineLogout />
+      </button>
     </div>
   );
 };
