1fff1bf49e0c51cc6e1830e9e897ef62d7efedcf
Ali nadhum <alinadhum1998@gmail.com>
2025-04-23 15:28:57 +0300

    Update homePage comp

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Container from "../../components/Container/Container";
 
 const HomePage = () => {
   return (
-    <div>
-      Home page
+    <div className='home'>
+      <Container>
+        <h1 className='text-3xl p-2 mt-5'>الرئيسيه</h1>
+      </Container>
     </div>
   )
 }
