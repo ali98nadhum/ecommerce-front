e6bc96501bedb2ce9476f54000b507e3fbf474c3
Ali nadhum <alinadhum1998@gmail.com>
2025-04-23 14:49:55 +0300

    update header

--- src/utils/Navbar/Navbar.jsx
+++ src/utils/Navbar/Navbar.jsx
@@ -4,35 +4,44 @@ import avatar from "../../assets/avatar.svg";
 import Container from "../../components/Container/Container";
 import { Link } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
+import { RiDiscountPercentLine } from "react-icons/ri";
 import Dropdown from "../dropdown/Dropdown";
 
 const Navbar = () => {
   const cartItemCount = 6;
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [user , setUser] = useState(true);
   return (
     <nav className="navbar p-2.5 h-20 w-full flex items-center shadow-md shadow-gray-400/30">
       <Container>
         <div className="flex items-center justify-between">
           <div className="left">
-            <Link>
-              {" "}
-              <img src={logo} alt="logo" className="w-16 h-16" />{" "}
+            <Link to={"/"}>
+              <img src={logo} alt="logo" className="w-16 h-16" />
             </Link>
           </div>
           <div className="center">
-            <ul className="flex items-center justify-between gap-4">
+            <ul className="flex items-center justify-center gap-4">
+            <li>
+                <Link to={"/"} className="font-medium flex items-center justify-between gap-1 hover:text-green-600 hover:scale-105">
+                  <RiDiscountPercentLine className="text-green-600 text-2xl" />
+                  <p>التخفيضات</p>
+                </Link>
+              </li>
               <li>
-                <Link to={"/"} className="font-medium">
+                <Link to={"/"} className="font-medium transition-all duration-300 hover:text-green-600 hover:scale-105">
                   المفضله
                 </Link>
               </li>
+             {user && (
+               <li>
+               <Link to={"/"} className="font-medium transition-all duration-300 hover:text-green-600 hover:scale-105">
+                 طلباتي
+               </Link>
+             </li>
+             )}
               <li>
-                <Link to={"/"} className="font-medium">
-                  طلباتي
-                </Link>
-              </li>
-              <li>
-                <Link to={"/"} className="font-medium">
+                <Link to={"/"} className="font-medium transition-all duration-300 hover:text-green-600 hover:scale-105">
                   الرئيسيه
                 </Link>
               </li>
