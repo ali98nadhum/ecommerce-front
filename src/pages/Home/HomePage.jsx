e6f77ddf92bce5cdd8f4456c92973c461ac13edf
Ali nadhum <alinadhum1998@gmail.com>
2025-04-26 19:10:49 +0300

    Update homePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -12,37 +12,37 @@ import ProductCard from "../../components/ProductCard/ProductCard";
 const products = [
   {
     name: "هاتف سامسونج",
-    price: "450,000",
+    price: "450",
     image:
       "https://images.samsung.com/is/image/samsung/assets/cl/home/2025/w6/BANNER-s25-P.png?imwidth=1366",
   },
   {
     name: "سوني بلايستيشن",
-    price: "600,000",
+    price: "600",
     image:
       "https://image.makewebeasy.net/makeweb/m_1920x0/TPguOW5od/Sony_page%2FBanner_Sony_03.jpg?v=202405291",
   },
   {
     name: "هواوي نوفا",
-    price: "380,000",
+    price: "380",
     image:
       "https://avestastorage.blob.core.windows.net/root/root/images/1318420222banner-fmc-huawei-logo.jpg",
   },
   {
     name: "ايفون 15",
-    price: "1,200,000",
+    price: "1,200",
     image:
       "https://as1.ftcdn.net/jpg/03/36/02/16/1000_F_336021608_AqJQDNtfEioWfCHLJrNbH4t13TaPfL6f.jpg",
   },
   {
     name: "ايفون 15",
-    price: "1,200,000",
+    price: "1,200",
     image:
       "https://as1.ftcdn.net/jpg/03/36/02/16/1000_F_336021608_AqJQDNtfEioWfCHLJrNbH4t13TaPfL6f.jpg",
   },
   {
     name: "ايفون 15",
-    price: "1,200,000",
+    price: "1,200",
     image:
       "https://as1.ftcdn.net/jpg/03/36/02/16/1000_F_336021608_AqJQDNtfEioWfCHLJrNbH4t13TaPfL6f.jpg",
   },
@@ -59,9 +59,6 @@ const products = [
       "https://as1.ftcdn.net/jpg/03/36/02/16/1000_F_336021608_AqJQDNtfEioWfCHLJrNbH4t13TaPfL6f.jpg",
   },
 ];
-
-
-
 
 const HomePage = () => {
   const { t, i18n } = useTranslation();
@@ -83,13 +80,12 @@ const HomePage = () => {
         <SectionTitleBar title={t("category-title")} link={"/category"} />
         <CardCarousel items={products} CardComponent={CategoriesCard} />
         {/* ===== Products besr siller */}
-        <SectionTitleBar title={t("best-seller")} link={"/"}/>
-        <ProductCarousel items={products} CardComponent={ProductCard}/>
-
-
-        </Container>
-        
-
+        <SectionTitleBar title={t("best-seller")} link={"/"} />
+        <ProductCarousel items={products} CardComponent={ProductCard} />
+        {/* ===== Products besr siller */}
+        <SectionTitleBar title={"التخفيضات"} link={"/"} />
+        <ProductCarousel items={products} CardComponent={ProductCard} />
+      </Container>
     </div>
   );
 };
