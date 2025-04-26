b48a583ad4977c0bd1c036c87a20945297e54f6e
Ali nadhum <alinadhum1998@gmail.com>
2025-04-26 19:41:09 +0300

    Update BrandCard comp

--- src/components/BrandCard/BrandCard.jsx
+++ src/components/BrandCard/BrandCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const BrandCard = ({item}) => {
     return (
-        <div className="w-24 sm:w-44 md:w-52 h-28 sm:h-52 overflow-hidden bg-white rounded-lg shadow-lg">
-        <img className="object-cover w-full h-20 sm:h-32" src={item.image} alt="avatar" />
+        <Link to={""}>
+        <div className="w-24 sm:w-44 md:w-52 h-28 sm:h-52 overflow- p-2 bg-white rounded-lg shadow-lg">
+        <img className="object-cover w-full h-full rounded-lg" src={item.image} alt="avatar" />
       </div>
+      </Link>
       );
 }
 
