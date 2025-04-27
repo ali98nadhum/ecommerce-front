832a8582a60cc09be1d78a3dfb44eb1a67eb7c63
Ali nadhum <alinadhum1998@gmail.com>
2025-04-28 02:01:54 +0300

    create TopHeader comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../src/utils/Navbar/Navbar";
 import HomePage from "../src/pages/Home/HomePage";
+import TopHeader from "../src/components/TopHeader/TopHeader";
 
 function App() {
   return (
     <>
+    <TopHeader/>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
