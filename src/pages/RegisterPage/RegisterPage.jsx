3bb28aec5057029d3cf4c4e92602388a2cdc2c29
Ali nadhum <alinadhum1998@gmail.com>
2025-05-01 21:40:41 +0300

    update RegisterPage

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React,{useEffect} from "react";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import { useTranslation } from "react-i18next";
 
 const RegisterPage = () => {
+     const { t, i18n } = useTranslation();
+        useEffect(() => {
+          document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
+        }, [i18n.language]);
   return (
     <div className="min-h-screen flex items-center justify-center px-4 mt-5 mb-14">
       <div className="max-w-md w-full bg-white p-8 rounded shadow-md">
-        <h2 className="text-2xl font-bold text-center mb-6">إنشاء حساب</h2>
+        <h2 className="text-2xl font-bold text-center mb-6"> {t("create-account")} </h2>
         <RegisterForm />
       </div>
     </div>
